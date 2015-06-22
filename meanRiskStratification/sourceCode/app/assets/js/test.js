dc61c5ee0e897092fcfbf047aea27829f8381a40
Tony Hall <tony.hall@nih.gov>
2015-06-22 12:37:42 -0400

    finalized population of test values into input fields

--- meanRiskStratification/sourceCode/app/assets/js/test.js
+++ meanRiskStratification/sourceCode/app/assets/js/test.js
@@ -1,38 +1,92 @@
-function test() {
-    var tbs = $('.collapse.in');
-    var values_option_1_bm = [{"markerName": "HPV", "a": 471, "b": 13, "c": 4680, "d": 25207},
-        {"markerName": "Pap", "a": 466, "b": 25, "c": 4484, "d": 25396},
-        {"markerName": "VIA", "a": 270, "b": 225, "c": 2967, "d": 26909}];
+var values_option_1_bm = [
+    {"markerName": "HPV", "a": 471, "b": 13, "c": 4680, "d": 25207},
+    {"markerName": "Pap", "a": 466, "b": 25, "c": 4484, "d": 25396},
+    {"markerName": "VIA", "a": 270, "b": 225, "c": 2967, "d": 26909}
+];
 
-    var values_option_2 = {"ppv": 0.0914, "npv": (1 - 0.0005), "P(M+)": 0.1696, "total": 30371};
-
-    $('#markers').children().each(function (key, value) {
-
-        if ($(this).find('.collapse.in')) {
-            var id = $(this).find('.collapse.in').prop('id');
-            if (id == "marker-1-panel-1") {
-                $(value).find('[name="name-input"]').val(values_option_1_bm[0].markerName);
-                $('#' + id).find('#a').val(values_option_1_bm[0].a);
-                $('#' + id).find('#b').val(values_option_1_bm[0].b);
-                $('#' + id).find('#c').val(values_option_1_bm[0].c);
-                $('#' + id).find('#d').val(values_option_1_bm[0].d);
-            }
-            if (id == "marker-2-panel-1") {
-                $(value).find('[name="name-input"]').val(values_option_1_bm[1].markerName);
-                $('#' + id).find('#a').val(values_option_1_bm[1].a);
-                $('#' + id).find('#b').val(values_option_1_bm[1].b);
-                $('#' + id).find('#c').val(values_option_1_bm[1].c);
-                $('#' + id).find('#d').val(values_option_1_bm[1].d);
-            }
-            if (id == "marker-3-panel-1") {
-                $(value).find('[name="name-input"]').val(values_option_1_bm[2].markerName);
-                $('#' + id).find('#a').val(values_option_1_bm[2].a);
-                $('#' + id).find('#b').val(values_option_1_bm[2].b);
-                $('#' + id).find('#c').val(values_option_1_bm[2].c);
-                $('#' + id).find('#d').val(values_option_1_bm[2].d);
-            }
-
+var values_option_2_bm = [{
+    "markerName": "HPV",
+    "ppv": 0.0914,
+    "npv": 0.95,
+    "prob_m": 0.1696,
+    "sampsize": 30371
+},
+    {
+        "markerName": "Pap",
+        "ppv": 0.0842,
+        "npv": 0.9,
+        "prob_m": 0.163,
+        "sampsize": 30371
+    },
+    {
+        "markerName": "VIA",
+        "ppv": 0.0834,
+        "npv": 0.17,
+        "prob_m": 0.1066,
+        "sampsize": 30371
+    }];
+function setup_test() {
+    $.when(reset).done(function () {
+        if ($('#markers').children().length != 1) {
+            reset();
         }
 
+        // add 2 biomarkers
+        new_marker();
+        new_marker();
+    });
+}
+
+function test() {
+    setup_test();
+    // capture what element triggered test function
+    var choice = $(this).prop('id');
+
+    $('#markers').children().each(function (key, markerElement) {
+        var id;
+
+        if (choice == "test1") {
+            // open option 1 for each
+            $(this).find('.collapse:first').addClass('in');
+            $(this).find('.collapse:last').removeClass('in');
+
+            id = $(this).find('.collapse.in').prop('id');
+
+            // then populate with data
+            $(markerElement).find('[name="name-input"]').val(values_option_1_bm[key].markerName);
+            $('#' + id).find('#a').val(values_option_1_bm[key].a);
+            $('#' + id).find('#b').val(values_option_1_bm[key].b);
+            $('#' + id).find('#c').val(values_option_1_bm[key].c);
+            $('#' + id).find('#d').val(values_option_1_bm[key].d);
+        }
+
+        if (choice == "test2") {
+            // collapse all option 1 panels and expand all option 2 panels
+            $(this).find('.collapse:last').addClass('in');
+            $(this).find('.collapse:first').removeClass('in');
+
+            id = $(this).find('.collapse.in').prop('id');
+
+            var data = [
+                values_option_2_bm[0],
+                values_option_2_bm[1],
+                values_option_2_bm[2]
+            ];
+            // marker name
+            $(markerElement).find('input[name="name-input"]').val(data[key].markerName);
+            // sample size
+            $(markerElement).find('input[name="sampsize"]').val(data[key].sampsize);
+
+            var dataItem = data[key];
+
+            //first row
+            $(markerElement).find('input[name="param_1"]').val(dataItem.ppv);
+
+            // second row
+            $(markerElement).find('input[name="param_2"]').val(dataItem.npv);
+
+            // third row
+            $(markerElement).find('input[name="param_3"]').val(dataItem.prob_m);
+        }
     });
 }
