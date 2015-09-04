090b2542dd6bf5df27af0341e95b5e74ca310bce
aasheer <aasheer1978@yahoo.com>
2015-09-04 11:26:02 -0400

    adding generated template to age interval section, save model button, and toggle functionality

--- absoluteRisk/js/services/ageIntervalSectionModel.js
+++ absoluteRisk/js/services/ageIntervalSectionModel.js
@@ -3,11 +3,13 @@ app.factory('BuildAgeIntervalModel', function() {
     function AgeIntervalModel() {
         var self = this;
 
+        self.genTemplate = true;
         self.inputMethod = 'manual';
         self.ages = arrRange(120);
         self.ageIntervals = arrRange(100);
         self.age = self.ages[29];
         self.ageInterval = self.ageIntervals[2];
+        self.placeHolderRows = [0, 1, 2, 3, 5];
 
         function arrRange(i){
             return i ? arrRange(i-1).concat(i):[];
