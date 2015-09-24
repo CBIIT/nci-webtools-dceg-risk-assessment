9f6fb119a48a6af221d7787ae8d9a700cdb0eb71
Brian Park <re.public@outlook.com>
2015-09-24 09:29:13 -0400

    using iCare library

--- absoluteRisk/rfiles/arcWrapper.R
+++ absoluteRisk/rfiles/arcWrapper.R
@@ -6,21 +6,7 @@ library(SparseM)
 library(Matrix)
 library(slam)
 library(modeest)
-#library(iCare)
-
-# source("./absoluteRiskCalculation.R")
-
-# lib = switch(Sys.info()['sysname'],
-#              Windows = 'source.dll',
-#              Linux = 'source.so')
-
-source("./rfiles/absoluteRiskCalculation.R")
-
-lib = switch(Sys.info()['sysname'],
-             Windows = './rfiles/source.dll',
-             Linux = './rfiles/source.so')
-
-dyn.load(lib)
+library(iCare)
 
 #-----------------------------------------
 # Function: convert the JSON data to RData
