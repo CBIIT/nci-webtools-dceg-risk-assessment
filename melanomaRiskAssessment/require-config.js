ba32555eb891604afa890ea42c7bf39dbb07bafa
Tony Hall <tony.hall@nih.gov>
2015-12-30 15:52:57 -0500

    using jquery.validate plugin

--- melanomaRiskAssessment/require-config.js
+++ melanomaRiskAssessment/require-config.js
@@ -3,8 +3,10 @@ requirejs.config({
     waitSeconds: 2,
     paths:{
         
-        jquery: ["https://code.jquery.com/jquery-1.11.3", "src/node_modules/jquery/dist/jquery"],
-        jqueryUI: ["https://code.jquery.com/ui/1.11.4/jquery-ui", "src/node_modules/jquery-ui/jquery-ui"],
+        jquery: [ "https://code.jquery.com/jquery-1.11.3", "src/node_modules/jquery/dist/jquery" ],
+        jqueryUI: [ "https://code.jquery.com/ui/1.11.4/jquery-ui", "src/node_modules/jquery-ui/jquery-ui" ],
+        "jquery.validate" : ["https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate", "src/node_modules/jquery-validation/dist/jquery.validate"],
+        "jquery.validate.methods" : [ "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods", "src/node_modules/jquery-validation/dist/additional-methods" ],
         globalGlossary:"/common/js/popover-functions",
         mrat: ["mrat"],
         navActions: "nav-actions",
@@ -13,6 +15,12 @@ requirejs.config({
     shim: {
         jqueryUI: {
             deps: ["jquery"],
+        },
+        "jquery.validate": {
+            deps: ["jquery"],
+        },
+        "jquery.validate.methods": {
+            deps: ["jquery.validate"],
         },
         globalGlossary: {
             // define what this script depends on in order to run
@@ -25,7 +33,7 @@ requirejs.config({
             deps: ["globalGlossary"],
         },
         mrat: {
-            deps: ["jquery"],
+            deps: ["jquery.validate.methods"],
         }
     }
 });
--- melanomaRiskAssessment/src/app/scripts/mrat.js
+++ melanomaRiskAssessment/src/app/scripts/mrat.js
@@ -1,66 +1,193 @@
 var genderChangeLoaded = false;
 function addClass(elementList,className) {
-  for (var index = 0; index < elementList.length; index++) {
-    elementList[index].className += " "+className;
-  }
+    for (var index = 0; index < elementList.length; index++) {
+        elementList[index].className += " "+className;
+    }
 }
 function removeClass(elementList,className) {
-  for (var index = 0; index < elementList.length; index++) {
-    elementList[index].className = elementList[index].className.replace(className,"").trim();
-  }
+    for (var index = 0; index < elementList.length; index++) {
+        elementList[index].className = elementList[index].className.replace(className,"").trim();
+    }
 }
 
 function genderChange() {
-  if (!genderChangeLoaded) {
-    if( document.all && !document.getElementsByTagName ){
-      document.getElementsByTagName = function( nodeName ) {
-        if( nodeName == '*' ) return document.all;
-        var result = [], rightName = new RegExp( nodeName, 'i' ), i;
-        for( i=0; i<document.all.length; i++ )
-          if( rightName.test( document.all[i].nodeName ) )
-            result.push( document.all[i] );
-        return result;
-      };
-    }
-    if( typeof document.getElementsByClassName !== "function"){
-      document.getElementsByClassName = function( className, nodeName ) {
-        var result = [], tag = nodeName||'*', node, seek, i;
-        if( document.evaluate ) {
-          seek = '//'+ tag +'[@class="'+ className +'"]';
-          seek = document.evaluate( seek, document, null, 0, null );
-          while( (node = seek.iterateNext()) )
-            result.push( node );
-        } else {
-          var rightClass = new RegExp( '(^| )'+ className +'( |$)' );
-          seek = document.getElementsByTagName( tag );
-          for( i=0; i<seek.length; i++ )
-            if( rightClass.test( (node = seek[i]).className ) )
-              result.push( seek[i] );
+    if (!genderChangeLoaded) {
+        if( document.all && !document.getElementsByTagName ){
+            document.getElementsByTagName = function( nodeName ) {
+                if( nodeName == '*' ) return document.all;
+                var result = [], rightName = new RegExp( nodeName, 'i' ), i;
+                for( i=0; i<document.all.length; i++ )
+                    if( rightName.test( document.all[i].nodeName ) )
+                        result.push( document.all[i] );
+                return result;
+            };
         }
-        return result;
-      };
+        if( typeof document.getElementsByClassName !== "function"){
+            document.getElementsByClassName = function( className, nodeName ) {
+                var result = [], tag = nodeName||'*', node, seek, i;
+                if( document.evaluate ) {
+                    seek = '//'+ tag +'[@class="'+ className +'"]';
+                    seek = document.evaluate( seek, document, null, 0, null );
+                    while( (node = seek.iterateNext()) )
+                        result.push( node );
+                } else {
+                    var rightClass = new RegExp( '(^| )'+ className +'( |$)' );
+                    seek = document.getElementsByTagName( tag );
+                    for( i=0; i<seek.length; i++ )
+                        if( rightClass.test( (node = seek[i]).className ) )
+                            result.push( seek[i] );
+                }
+                return result;
+            };
+        }
+        var lastClass = "select";
+        document.getElementById('gender').onchange = function() {
+            var className = this.value.toLowerCase();
+            var addList;
+            var removeList;
+            if (lastClass !== "select") {
+                removeClass(document.getElementsByClassName(lastClass),"show");
+            }
+            if (className !== "select") {
+                addClass(document.getElementsByClassName(className),"show");
+            }
+            lastClass = className;
+        };
+        genderChangeLoaded = true;
     }
-    var lastClass = "select";
-    document.getElementById('gender').onchange = function() {
-      var className = this.value.toLowerCase();
-      var addList;
-      var removeList;
-      if (lastClass !== "select") {
-        removeClass(document.getElementsByClassName(lastClass),"show");
-      }
-      if (className !== "select") {
-        addClass(document.getElementsByClassName(className),"show");
-      }
-      lastClass = className;
-    };
-    genderChangeLoaded = true;
-  }
 }
 
 window.onload = genderChange();
 $(genderChange());
 
+window.onsubmit = validate();
 
+
+var validationRules = {
+    state: {
+        required: true
+    },
+    county: {
+        required: {
+            depends: function(el) {
+
+            }
+        }
+    },
+    gender: {
+        required: true
+    },
+    race: {
+        required: true
+    },
+    age: {
+        required: true
+    },
+    sunburn: {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    },
+    complexion: {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    },
+    tan: {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    },
+    "big-moles": {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    },
+    "small-moles": {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    },
+    freckling: {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    },
+    damage: {
+        required:{
+            depends: function(el) {
+
+            }
+        }
+    }
+
+};
+var validationMessages = {
+    state: {
+        required: "State is required"
+    },
+    county: {
+        required: "County is required"
+    },
+    gender: {
+        required: "The patient's Gender is required"
+    },
+    race: {
+        required: "The patient's Race is required"
+    },
+    age: {
+        required: "The patient's Age is required"
+    },
+    sunburn: {
+        required: "You must provide an answer to the sunburn question"
+    },
+    complexion: {
+        required: "You must provide an answer to the complexion question"
+    },
+    tan: {
+        required: "You must provide an answer to the tanning question"
+    },
+    "big-moles": {
+        required: "You must answer question 'How many moles larger than 5mm in diameter are on the patient's back?'"
+    },
+    "small-moles": {
+        required: "You must answer question 'How many moles less than or equal to 5mm in diameter are on the patient's back?'"
+    },
+    freckling: {
+        required: "You must answer question 'How extensive is the freckling on the patient's back and shoulders?'"
+    },
+    damage: {
+        required: "You must answer question 'Does the patient have severe solar damage on the shoulders?'"
+    }
+
+};
+function validate(){
+    $(document.forms.survey).validate({
+        rules: validationRules,
+        messages: validationMessages,
+        errorPlacement: function(error, element) {
+        },
+        success: function(label) {
+        },
+        highlight: function(element, errorClass) {
+        },
+        submitHandler: function(form) {
+            $(form).submit();
+        }
+    });
+}
 $("#state").on("change", regionFilter);
 
 function regionFilter() {
