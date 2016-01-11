8780cdf1fc0eae771ddf09c306357f3ce4f76dce
Tony Hall <tony.hall@nih.gov>
2016-01-11 16:29:49 -0500

    expanding/collapsing image on click

--- melanomaRiskAssessment/src/app/scripts/mrat.js
+++ melanomaRiskAssessment/src/app/scripts/mrat.js
@@ -58,32 +58,32 @@ function genderChange() {
 }
 
 function displayResult(result) {
-  var matches = Number(result).toExponential().toString().match(/(\d+)(?:.(\d+))?e([-+]?\d+)/i);
-  var estimate = matches[1];
-  var outOf = 2-Number(matches[3]);
-  if (matches[2] !== undefined) {
-    estimate += matches[2];
-    outOf += matches[2].length;
-  }
-  outOf = Number('1e'+outOf);
-  $('#result').append('<h2>'+result+'%</h2><p>Your risk of developing cancer in the next 5 years is '+result+'%. This means that roughly '+estimate+' in '+outOf+' people like you are likely to develop cancer in the next 5 years.').removeClass('hide');
-  graphResult($('#result').append('<div class="chart"></div>').children('.chart'),Number(result));
+    var matches = Number(result).toExponential().toString().match(/(\d+)(?:.(\d+))?e([-+]?\d+)/i);
+    var estimate = matches[1];
+    var outOf = 2-Number(matches[3]);
+    if (matches[2] !== undefined) {
+        estimate += matches[2];
+        outOf += matches[2].length;
+    }
+    outOf = Number('1e'+outOf);
+    $('#result').append('<h2>'+result+'%</h2><p>Your risk of developing cancer in the next 5 years is '+result+'%. This means that roughly '+estimate+' in '+outOf+' people like you are likely to develop cancer in the next 5 years.').removeClass('hide');
+    graphResult($('#result').append('<div class="chart"></div>').children('.chart'),Number(result));
 }
 
 function graphResult(element,result) {
-  for (var i = 0; i < 100; i++) {
-    $(element).append('<img src="images/person.svg"/>');
-  }
-  var fullBars = Math.floor(result/20);
-  var partialBar = (result-(20*fullBars))*5;
-  var top = 0;
-  for (var j = 0; j < fullBars; i++) {
-    $(element).prepend('<div class="bar yours" style="top:'+top+'%;width:100%;"></div>');
-    top += 20;
-  }
-  if (partialBar > 0) {
-    $(element).prepend('<div class="bar yours" style="top:'+top+'%;width:'+partialBar+'%;"></div>');
-  }
+    for (var i = 0; i < 100; i++) {
+        $(element).append('<img src="images/person.svg"/>');
+    }
+    var fullBars = Math.floor(result/20);
+    var partialBar = (result-(20*fullBars))*5;
+    var top = 0;
+    for (var j = 0; j < fullBars; i++) {
+        $(element).prepend('<div class="bar yours" style="top:'+top+'%;width:100%;"></div>');
+        top += 20;
+    }
+    if (partialBar > 0) {
+        $(element).prepend('<div class="bar yours" style="top:'+top+'%;width:'+partialBar+'%;"></div>');
+    }
 }
 
 
@@ -194,33 +194,33 @@ function validate(){
             $('#result').addClass('hide').empty();
             $('.error').removeClass('error');
             $.ajax({
-              url: form.action,
-              type: form.method,
-              data: new FormData(form),
-              processData: false,
-              contentType: false,
-              dataType: 'json'
+                url: form.action,
+                type: form.method,
+                data: new FormData(form),
+                processData: false,
+                contentType: false,
+                dataType: 'json'
             }).done(function(data) {
-              if (data.success) {
-                displayResult(data.message);
-              } else {
-                var message = "";
-                if (data.message) {
-                  message += "<p>" + data.message + "</p>";
+                if (data.success) {
+                    displayResult(data.message);
+                } else {
+                    var message = "";
+                    if (data.message) {
+                        message += "<p>" + data.message + "</p>";
+                    }
+                    var index;
+                    for (index in data.missing) {
+                        $('#'+data.missing[index]).addClass('error');
+                        message += "<p>The " + data.missing[index] + " question was not answered.</p>";
+                    }
+                    for (index in data.nonnumeric) {
+                        $('#'+data.nonnumeric[index]).addClass('error');
+                        message += "<p>The " + data.missing[index] + " question contained a nonnumeric answer.</p>";
+                    }
+                    $('#error').append(message).css('display','block');
                 }
-                var index;
-                for (index in data.missing) {
-                  $('#'+data.missing[index]).addClass('error');
-                  message += "<p>The " + data.missing[index] + " question was not answered.</p>";
-                }
-                for (index in data.nonnumeric) {
-                  $('#'+data.nonnumeric[index]).addClass('error');
-                  message += "<p>The " + data.missing[index] + " question contained a nonnumeric answer.</p>";
-                }
-                $('#error').append(message).css('display','block');
-              }
             }).fail(function(data) {
-              $('#error').append("<p>An unknown error occurred. Please consult the administrator.</p>");
+                $('#error').append("<p>An unknown error occurred. Please consult the administrator.</p>");
             });
         }
     });
@@ -240,3 +240,13 @@ function regionFilter() {
 window.onload = genderChange();
 $(genderChange);
 $(validate);
+
+$(".thumbnail").on("click", function() {
+    $(this).toggleClass(function() {
+        if($(this).hasClass("expand"))
+            return;
+        else
+            this.scrollIntoView();
+            return "expand";
+    });
+});
