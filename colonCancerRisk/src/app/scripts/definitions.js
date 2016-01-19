30df726bc2f7462577547bbd178db461e3d5d99a
Tony Hall <tony.hall@nih.gov>
2016-01-19 11:50:54 -0500

    implementing term definitions

--- colonCancerRisk/src/app/scripts/definitions.js
+++ colonCancerRisk/src/app/scripts/definitions.js
@@ -1,4 +1,36 @@
 var terms = {
+    vegetable: {
+        fullName: "Vegetables",
+        definition: "INCLUDE raw, cooked, canned, and frozen vegetables (including beans) and leafy green salads. \n DO NOT INCLUDE fried vegetables like French fries or fried potatoes."
+    },
+    colon: {
+        fullName: "Colonoscopy",
+        definition: "Examination of the inside of the colon using a colonoscope, inserted into the rectum. A colonoscope is a thin, tube-like instrument with a light and a lens for viewing. It may also have a tool to remove tissue to be checked under a microscope for signs of disease."
+    },
+    sig: {
+        fullName: " Sigmoidoscopy",
+        definition: "Examination of the lower colon using a sigmoidoscope, inserted into the rectum. A sigmoidoscope is a thin, tube-like instrument with a light and a lens for viewing. It may also have a tool to remove tissue to be checked under a microscope for signs of disease. Also called proctosigmoidoscopy."
+    },
+    polyp: {
+        fullName: "Polyp",
+        definition: "A polyp is a small growth, like a skin tag, that develops on the inside of the colon or rectum."
+    },
+    asprin: {
+        fullName: "Medications containing Asprin",
+        definition: "Bufferin, Bayer, Excedrin or Other generic form"
+    },
+    "non-asprin": {
+        fullName: "Medications NOT containing Asprin",
+        definition: "Advil, Aleve, Celebrex, Ibuprofen, Motrin, Naproxen or Nuprin"
+    },
+    race_info: {
+        fullName: "",
+        definition: "When we first developed this tool, we tested it with white people and found it to be accurate in estimating their risk of colorectal cancer. If you are African American, Asian American/Pacific Islander, or Hispanic/Latino, this tool can still estimate your risk. But, because there is not as much data available for these groups, your results may be less accurate.\n In the future, we hope to make this tool more accurate for African Americans, Asian Americans/Pacific Islanders, and Hispanics/Latinos, as researchers complete studies that will provide information to update the tool for these groups.\n Although the tool is not perfect, the information you learn from using it can still help you and your doctor better understand your risk of colorectal cancer."
+    },
+    test: {
+        fullName: "Placeholder",
+        definition: "(text here)"
+    },
     test: {
         fullName: "Placeholder",
         definition: "(text here)"
