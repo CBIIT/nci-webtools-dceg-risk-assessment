3779dca6be775e827601b78ba408fc2473717f50
Tony Hall <tony.hall@nih.gov>
2016-02-02 17:11:56 -0500

    initial check in of bcrat merged content

---
+++ breastCancerRisk/src/app/scripts/bcrat.js
@@ -0,0 +1,68 @@
+var validationRules = {
+
+};
+var validationMessages = {
+};
+function validate(){
+    $(document.forms.survey).validate({
+        rules: validationRules,
+        messages: validationMessages,
+        errorLabelContainer: '#error',
+        wrapper: 'p',
+        submitHandler: function(form) {
+            $('#error').empty().css('display','none');
+            $('#result').addClass('hide').empty();
+            $('.error').removeClass('error');
+            $.ajax({
+                url: form.action,
+                type: form.method,
+                data: new FormData(form),
+                processData: false,
+                contentType: false,
+                dataType: 'json'
+            }).done(function(data) {
+                if (data.success) {
+                    displayResult(data.message);
+                } else {
+                    var message = "";
+                    if (data.message) {
+                        message += "<p>" + data.message + "</p>";
+                    }
+                    var index;
+                    for (index in data.missing) {
+                        $('#'+data.missing[index]).addClass('error');
+                        message += "<p>The " + data.missing[index] + " question was not answered.</p>";
+                    }
+                    for (index in data.nonnumeric) {
+                        $('#'+data.nonnumeric[index]).addClass('error');
+                        message += "<p>The " + data.missing[index] + " question contained a nonnumeric answer.</p>";
+                    }
+                    $('#error').append(message).css('display','block');
+                    document.getElementById("top").scrollIntoView();
+                }
+            }).fail(function(data) {
+//                $('#error').append("<p>An unknown error occurred. Please consult the administrator.</p>");
+                if(data.responseJSON.message)
+                    $('#error').append("<p>"+data.responseJSON.message+"</p>").css('display','block');
+                    document.getElementById("top").scrollIntoView();
+            });
+        }
+    });
+}
+
+$(validate);
+
+$(".thumbnail").on("click", function() {
+    $(this).toggleClass(function() {
+        if($(this).hasClass("expand"))
+            return;
+        else
+            return "expand";
+    });
+    this.scrollIntoView();
+});
+
+$("#reset").on("click", function(){
+    $(survey).validate().resetForm();
+    $(".show").removeClass("show");
+});
