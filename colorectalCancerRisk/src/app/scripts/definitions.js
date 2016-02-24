a6a6b4f88f2bd92777d06978657fd52303b94afd
Tony Hall <tony.hall@nih.gov>
2016-02-24 15:10:35 -0500

    reorganizing content, consistent look and feel based on mrat

--- colorectalCancerRisk/src/app/scripts/definitions.js
+++ colorectalCancerRisk/src/app/scripts/definitions.js
@@ -23,10 +23,6 @@ var terms = {
         fullName: "Medications NOT containing Asprin",
         definition: "Ex. Advil, Aleve, Celebrex, Ibuprofen, Motrin, Naproxen or Nuprin"
     },
-    race_info: {
-        fullName: "",
-        definition: "When we first developed this tool, we tested it with white people and found it to be accurate in estimating their risk of colorectal cancer. If you are African American, Asian American/Pacific Islander, or Hispanic/Latino, this tool can still estimate your risk. But, because there is not as much data available for these groups, your results may be less accurate.\n In the future, we hope to make this tool more accurate for African Americans, Asian Americans/Pacific Islanders, and Hispanics/Latinos, as researchers complete studies that will provide information to update the tool for these groups.\n Although the tool is not perfect, the information you learn from using it can still help you and your doctor better understand your risk of colorectal cancer."
-    },
     hrt: {
         fullName: "Hormone Replacement Therapy (HRT)",
         definition: "Hormones (estrogen, progesterone, or both) given to women after menopause to replace the hormones no longer produced by the ovaries. Also called HRT and menopausal hormone therapy."
