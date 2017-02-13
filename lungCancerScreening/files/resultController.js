4d834cc517737c9333e5060a1aa82e5a8e8a29a9
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2017-02-13 14:10:33 -0500

    testing

--- lungCancerScreening/files/resultController.js
+++ lungCancerScreening/files/resultController.js
@@ -79,15 +79,21 @@ app.controller("ResultCtrl", function($scope, $window, $sce, $http, $localStorag
     var cellArray = [];
     var html = '<table cellspacing="0" cellpadding="0" border="1">';
 
+    var row = [];
+    for (var z = 0; z < 40; z++) {
+      row.push('<th></th>');
+    }
+    cellArray.push(row);
+
     /* create rows and columns filled in with color based on units until units is zero */
     for (var x=0; x<25; x++) {
       var row = [];
       for (var z=1; z<41; z++) {
         if (units>0) {
-          row.push('<td class="results_cell" style="border: 1px solid #777; width:10px !important; min-width:10px;"><img src="' + $scope.base_url + 'files/cellfill.png" title="filled cell" alt="filled cell" /></td>');
+          row.push('<td style="border: 1px solid #777; width:10px !important; min-width:10px;"><img src="' + $scope.base_url + 'files/cellfill.png" title="filled cell" alt="filled cell" /></td>');
         }
         else {
-          row.push('<td class="results_cell" style="border: 1px solid #777; width:10px !important; min-width:10px;"><img src="' + $scope.base_url + 'files/cellempty.png" title="empty cell" alt="empty cell" /></td>');
+          row.push('<td style="border: 1px solid #777; width:10px !important; min-width:10px;"><img src="' + $scope.base_url + 'files/cellempty.png" title="empty cell" alt="empty cell" /></td>');
         };
         units-=1;
       };
