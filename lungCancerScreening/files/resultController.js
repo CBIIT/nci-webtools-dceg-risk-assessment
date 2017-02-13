fbeab45f597be8b62d914d0687dbfcc2c169a84b
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2017-02-13 11:55:51 -0500

    xhtml compleiton fixes

--- lungCancerScreening/files/resultController.js
+++ lungCancerScreening/files/resultController.js
@@ -153,8 +153,11 @@ app.controller("ResultCtrl", function($scope, $window, $sce, $http, $localStorag
     var source  = $("#results .ng-hide").remove()
     var results = $("#results").html()
     results = results.replace(new RegExp('<br>', 'g'), '<br />');
-    results = results.replace(new RegExp('alt="filled cell"', 'g'), 'alt="filled cell" /');
-    results = results.replace(new RegExp('alt="empty cell"', 'g'), 'alt="empty cell" /');
+    results = results.replace(new RegExp('alt="filled cell">', 'g'), 'alt="filled cell" />');
+    results = results.replace(new RegExp('alt="empty cell">', 'g'), 'alt="empty cell" />');
+    results = results.replace(new RegExp('cellempty.png">', 'g'), 'cellempty.png"/>');
+    results = results.replace(new RegExp('cellfill.png">', 'g'), 'cellfill.png"/>');
+
 
     html+= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
     html+= '<html xmlns="http://www.w3.org/1999/xhtml">';
