bb9abd12d68cd9a6f025cb1a3b2063c3b63e7fae
Charles Stockman <charles.stockman@nih.gov>
2018-01-08 17:56:45 -0500

    Problem : From the python northern was spelt northernern.  I fixed the problem by fixing the indetation

--- melanomarisktool/mratRest.py
+++ melanomarisktool/mratRest.py
@@ -122,8 +122,8 @@ class MelanomaRiskAssessmentTool:
             for key, value in MratConstants.RegionIndex.items():
                 if value is region:
                     regionKey = key
-                if value is not 1:
-                    regionKey = regionKey + "ern"
+                    if value is not 1:
+                        regionKey = regionKey + "ern"
 
             result = "The Five-Year Absolute Risk of Melanoma is {0}%. For every 1,000 {1}s living in the {2} region with these characteristics, on average about {3} will develop melanoma in the next 5 years.".format(
                 risk, str(parameters['gender'] ).lower(), regionKey, int(ratio) )
