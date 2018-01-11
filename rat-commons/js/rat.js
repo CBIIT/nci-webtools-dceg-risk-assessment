425022eb4a31163f266c00d1c5b7a0267c0ef1ec
Charles Stockman <charles.stockman@nih.gov>
2018-01-11 14:09:16 -0500

    An emergency fix: The hide() and show() were from jquery were somehow swallowing an event
    	Problem : When the header was invisibile ( as it should have been ) there was a large white rectangle
    
    		Analysis : visibile only make the object invisible, but still calculates the size where as display:none make the object invisible and does not calculate
    		the size for it.
    
    we replaced it with the css code.

--- rat-commons/js/rat.js
+++ rat-commons/js/rat.js
@@ -333,11 +333,11 @@ function fixedToTop(div,use_mobile) {
 
 			if ( window_top > 0 )	{
 				header_height = 0
-				$("header").css("visibility", "show")
+				$("header").css("display", "none")
 			}
 			else {
 				$("header").css("top", "0px")
-				$("header").css("visibility", "hidden")
+				$("header").css("display", "block")
 			}
 
  			$("#form-steps").css("top",header_height+"px");
