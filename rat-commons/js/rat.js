991b697762bb9a988ef5fc1bb3d987533f47f3ca
Stockman <stockmanc2@nci-01962126-ml.nci.nih.gov>
2018-01-23 13:48:06 -0500

    GOV-5794 : Remove Print Icon from Mobile
    	Fixed the Spacing for the back to results

--- rat-commons/js/rat.js
+++ rat-commons/js/rat.js
@@ -141,6 +141,13 @@ function go_toresult() {
 	else if ( isMobile() )
 		$("#results_home").css("margin-top", "116px");
 
+	// This code is a hack and this should be done in CSS.  I put the code in 
+	// here since we are trying to get it done and it might have unforseen 
+	// consequences if put in the CSS Style
+	if ( isTablet() ) {
+		$("#results_home").css("padding-top", "6px")
+	}
+
 }
 
 /*********************************************************************************/
