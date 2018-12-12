be5e6a40a61ac4d01e8cac062752981477b9ba27
Ming Ying <yingming18@gmail.com>
2018-12-12 15:52:51 -0500

    Move chart data transformation into chart directives, Make "False alarm" orange.

--- lungCancerScreening/files/FormController.js
+++ lungCancerScreening/files/FormController.js
@@ -336,39 +336,8 @@ app.controller("FormCtrl", function ($scope, $sce, $http, $sessionStorage, $loca
       /* Round to 2 decimal places and assign results to UI properties */
       /* $scope.myForm['result' + i] = Math.round(data[i] * 100) / 100; */
       $scope.myForm['result' + i] = Math.round(data[i]);
-      $scope.myForm['resultMatrix' + i] = $util.numToMatrix([$scope.myForm['result' + i] / 10]);
 
       /* GLOBAL_RESULTS['result' + i] = $scope.myForm['result' + i]; */
-    }
-
-    $scope.myForm.allResults = [];
-
-    if ($scope.myForm.result0 && $scope.myForm.result1) {
-      var result0_1 = ($scope.myForm.result0 - $scope.myForm.result1) / 10;
-      $scope.myForm['resultMatrix0_1'] = $util.numToMatrix([result0_1]);
-      $scope.myForm.allResults.push(result0_1);
-      $scope.myForm.allResults.push($scope.myForm.result0 / 10);
-      if ($scope.myForm.result4) {
-          $scope.myForm['resultMatrixCombined1'] = $util.numToMatrix([$scope.myForm.result0 / 10, result0_1]);
-          $scope.myForm['resultMatrixCombined3'] = $util.numToMatrix([0, $scope.myForm.result4 / 10]);
-          $scope.myForm['resultMatrixCombined3b'] = $util.numToMatrix([$scope.myForm.result4], 25, 40);
-
-          $scope.myForm['resultMatrixCombined1b'] = $util.numToMatrix([$scope.myForm.result0 - $scope.myForm.result1, 0], 25, 40);
-          $util.combineMatrices($scope.myForm['resultMatrixCombined1b'], $util.numToMatrix([0, $scope.myForm.result0], 25, 40));
-      }
-
-    }
-    if ($scope.myForm.result2 && $scope.myForm.result3) {
-      var result2_3 = ($scope.myForm.result2 + $scope.myForm.result3) / 10;
-      $scope.myForm['resultMatrix2_3'] = $util.numToMatrix([result2_3]);
-      $scope.myForm['resultMatrixCombined2'] = $util.numToMatrix([result2_3,  $scope.myForm.result2 / 10]);
-      $scope.myForm['resultMatrixCombined2b'] = $util.numToMatrix([$scope.myForm.result2, 0], 25, 40);
-      $util.combineMatrices($scope.myForm['resultMatrixCombined2b'], $util.numToMatrix([0, $scope.myForm.result2 + $scope.myForm.result3], 25, 40));
-      $scope.myForm.allResults.push($scope.myForm.result2 / 10);
-      $scope.myForm.allResults.push(result2_3);
-    }
-    if ($scope.myForm.result4) {
-      $scope.myForm.allResults.push($scope.myForm.result4 / 10);
     }
 
     if ($scope.myForm.result0 > $scope.myForm.result2) {
--- lungCancerScreening/files/chartDirective.js
+++ lungCancerScreening/files/chartDirective.js
@@ -1,42 +1,47 @@
 'use strict';
 
-app.directive('lcsChart', function() {
+app.directive('lcsChart', ['$util', function($util) {
     return {
         scope: {
-            chartType : '=',
-            rows: '=value'
+            values: '='
         },
         controller: function($scope) {
             $scope.iconWidth = 15;
+            $scope.rows = $util.numToMatrix($scope.values.map(function(val) {
+                    return val / 10;
+                }));
         },
         restrict: 'E',
         templateUrl: '_chart.html'
     }
-});
+}]);
 
-app.directive('bathroomChart', function() {
+app.directive('bathroomChart', ['$util', function($util) {
     return {
         scope: {
-            rows: '=value'
+            values: '='
         },
         controller: function($scope) {
             $scope.iconWidth = 15;
+            $scope.rows = $util.numToMatrix($scope.values.map(function(val) {
+                return val / 10;
+            }));
         },
         restrict: 'E',
         templateUrl: '_bathroomChart.html'
     }
-});
+}]);
 
-app.directive('lcsChart2', function() {
+app.directive('lcsChart2', ['$util', function($util) {
     return {
         scope: {
-            chartType : '=',
-            rows: '=value'
+            values: '='
         },
         controller: function($scope) {
             $scope.iconWidth = 12;
+            $scope.rows = $util.numToMatrix($scope.values, 25, 40);
         },
         restrict: 'E',
         templateUrl: '_chart2.html'
     }
-});
+}]);
