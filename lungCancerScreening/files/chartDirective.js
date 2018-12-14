c9eebf4aba1bb1baf2ff7913f490e2cdc9504502
Ming Ying <yingming18@gmail.com>
2018-12-14 13:25:55 -0500

    Change all charts to use canvas.

--- lungCancerScreening/files/chartDirective.js
+++ lungCancerScreening/files/chartDirective.js
@@ -1,59 +1,27 @@
 'use strict';
 
-app.directive('lcsChart', ['$util', function($util) {
-    return {
-        restrict: 'E',
-        scope: {
-            values: '='
-        },
-        templateUrl: '_chart.html',
-        controller: function($scope) {
-            $scope.iconWidth = 15;
-            $scope.rows = $util.numToMatrix($scope.values.map(function(val) {
-                    return val / 10;
-                }));
-        }
-    }
-}]);
-
-app.directive('bathroomChart', ['$util', function($util) {
-    return {
-        restrict: 'E',
-        scope: {
-            values: '='
-        },
-        templateUrl: '_bathroomChart.html',
-        controller: function($scope) {
-            $scope.iconWidth = 15;
-            $scope.rows = $util.numToMatrix($scope.values.map(function(val) {
-                return val / 10;
-            }));
-        }
-    }
-}]);
-
-
 app.directive('lcsChart2', ['$util', function($util) {
     return {
         restrict: 'AC',
         scope: {
             values: '='
         },
-        templateUrl: '_chart2.html',
         controller: function($scope) {
-            $scope.nRow = 25;
-            $scope.nCol = 40;
-            $scope.iconWidth = 12;
-            $scope.iconHeight = 30;
+            $scope.nRow = 10;
+            $scope.nCol = 10;
+            $scope.iconWidth = 15;
+            $scope.iconHeight = 25;
+            $scope.textWidth = 35;
+            $scope.textHeight = 15;
             $scope.gap = {
-                x: 2,
-                y: 2
+                x: 6,
+                y: 6
             };
+            $scope.textColor = '#777777';
             $scope.color0 = '#aeb0b5';
             $scope.color1 = '#fdb81e';
             $scope.color2 = '#0071bc';
             $scope.color3 = '#e31c3d';
-            $scope.rows = $util.numToMatrix($scope.values, $scope.nRow, $scope.nCol);
 
             $scope.getPosition = function(row, col, x, y, width, height, gap) {
                 return {
@@ -63,19 +31,23 @@ app.directive('lcsChart2', ['$util', function($util) {
                 };
             };
             $scope.calculateCanvasWidth = function() {
-                return ($scope.iconWidth + $scope.gap.x) * $scope.nCol + $scope.gap.x;
+                return $scope.textWidth + ($scope.iconWidth + $scope.gap.x) * ($scope.nCol + 1) + $scope.gap.x * 2;
             };
             $scope.calculateCanvasHeight = function() {
                 return ($scope.iconHeight + $scope.gap.y) * $scope.nRow + $scope.gap.y;
             };
 
             $scope.drawChart = function() {
+                $scope.rows = $util.numToMatrix($scope.values, $scope.nRow, $scope.nCol);
                 var savedColor = $scope.ctx.fillStyle;
 
                 for (var i = 0; i < $scope.rows.length; ++i) {
                     var row = $scope.rows[i];
+                    var pos = $scope.getPosition(i, 0, $scope.gap.x + $scope.textWidth, $scope.gap.y + ($scope.iconHeight + $scope.textHeight) / 2, $scope.textWidth, $scope.iconHeight, $scope.gap);
+                    $scope.ctx.fillStyle = $scope.textColor;
+                    $scope.ctx.fillText(i * $scope.nCol + $scope.nCol, pos.x, pos.y);
                     for (var j = 0; j < row.length; ++j) {
-                        var pos = $scope.getPosition(i, j, $scope.gap.x, $scope.gap.y, $scope.iconWidth, $scope.iconHeight, $scope.gap);
+                        pos = $scope.getPosition(i, j, $scope.gap.x * 2 + $scope.textWidth, $scope.gap.y, $scope.iconWidth, $scope.iconHeight, $scope.gap);
                         $scope.ctx.fillStyle = $scope.color0;
                         $scope.ctx.fillRect(pos.x, pos.y, $scope.iconWidth, $scope.iconHeight);
                         if (row[j].val1 > 0) {
@@ -90,17 +62,43 @@ app.directive('lcsChart2', ['$util', function($util) {
                             $scope.ctx.fillStyle = $scope.color3;
                             $scope.ctx.fillRect(pos.x, pos.y, $scope.iconWidth * row[j].val3 / 10, $scope.iconHeight);
                         }
-                        $scope.ctx.drawImage($scope.img, pos.x, pos.y, $scope.iconWidth, $scope.iconHeight);
+                        if ($scope.drawImage) {
+                            $scope.ctx.drawImage($scope.img, pos.x, pos.y, $scope.iconWidth, $scope.iconHeight);
+                        }
                     }
                 }
                 $scope.ctx.fillStyle = savedColor;
             };
         },
         link: function($scope, element, attrs) {
+            if (attrs.drawImage) {
+                $scope.drawImage = attrs.drawImage;
+            }
+            if (attrs.nRow) {
+                $scope.nRow = parseInt(attrs.nRow);
+            }
+            if (attrs.nCol) {
+                $scope.nCol = parseInt(attrs.nCol);
+            }
+            if (attrs.iconWidth) {
+                $scope.iconWidth = parseInt(attrs.iconWidth);
+            }
+            if (attrs.iconHeight) {
+                $scope.iconHeight = parseInt(attrs.iconHeight);
+            }
+            if (attrs.gapX) {
+                $scope.gap.x = parseInt(attrs.gapX);
+            }
+            if (attrs.gapY) {
+                $scope.gap.y = parseInt(attrs.gapY);
+            }
+
             if (element[0].getContext) {
                 element[0].width = $scope.calculateCanvasWidth();
                 element[0].height = $scope.calculateCanvasHeight();
                 $scope.ctx = element[0].getContext('2d');
+                $scope.ctx.textAlign = 'right';
+                $scope.ctx.font = 'bold ' + $scope.textHeight + 'px sans-serif';
                 var img = new Image();
                 img.onload = function() {
                     $scope.img = img;
