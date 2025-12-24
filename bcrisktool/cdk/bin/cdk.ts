3a34ea7b6d13d18acec52f2940cd919e70a17010
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2025-12-24 13:13:28 -0500

    add cdk folder for bcrisktool

---
+++ bcrisktool/cdk/bin/cdk.ts
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { EcrStack } from '../lib/ecr-stack';
+import { EcsStack } from '../lib/ecs-stack';
+import { ParametersStack } from '../lib/parameters-stack';
+
+const app = new cdk.App();
+
+// Get context variables
+const envName = app.node.tryGetContext('env') || 'dev';
+const createEcr = app.node.tryGetContext('create_ecr') || false;
+const containerImage = app.node.tryGetContext('container_image');
+const releaseVersion = app.node.tryGetContext('release_version');
+const lastUpdated = app.node.tryGetContext('last_updated');
+const awsAccount = process.env.CDK_DEFAULT_ACCOUNT;
+const awsRegion = process.env.CDK_DEFAULT_REGION || 'us-east-1';
+
+const env = {
+  account: awsAccount,
+  region: awsRegion,
+};
+
+// Configuration
+const appName = 'bcrisktool';
+const stackName = `analysistools-${envName}-${appName}`;
+
+// Common tags
+const commonTags = {
+  Application: appName,
+  Environment: envName,
+  ManagedBy: 'CDK',
+};
+
+// ECR Stack (optional - set create_ecr context to true if you want CDK to manage ECR)
+let ecrStack: EcrStack | undefined;
+if (createEcr) {
+  ecrStack = new EcrStack(app, `${stackName}-ecr`, {
+    appName,
+    envName,
+    env,
+    tags: commonTags,
+  });
+}
+
+// ECS Stack
+const ecsStack = new EcsStack(app, `${stackName}-ecs`, {
+  appName,
+  envName,
+  stackName,
+  containerImage,
+  releaseVersion,
+  lastUpdated,
+  env,
+  tags: commonTags,
+  // Optional: Configure VPC, subnets, ALB
+  // vpcId: 'vpc-xxxxx',
+  // subnetIds: ['subnet-xxxxx', 'subnet-yyyyy'],
+  // albListenerArn: 'arn:aws:elasticloadbalancing:...',
+  // healthCheckPath: '/',
+});
+
+// Parameters Stack
+const parametersStack = new ParametersStack(app, `${stackName}-parameters`, {
+  appName,
+  envName,
+  ecsClusterName: ecsStack.cluster.clusterName,
+  ecsWebTaskFamily: ecsStack.webTaskDefinition.family,
+  ecsWebServiceName: ecsStack.webService.serviceName,
+  taskRoleArn: ecsStack.taskRole.roleArn,
+  env,
+  tags: commonTags,
+});
+
+parametersStack.addDependency(ecsStack);
+
+app.synth();
